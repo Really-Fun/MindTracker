02a88d47ca4b7dcb7ceded4b70756a222fa31555
ReallyFun <ilyareztcov@gmail.com>
2026-03-11 18:58:30 +0300

    add: custon label in admin

--- MindTracker/UserTracker/admin.py
+++ MindTracker/UserTracker/admin.py
@@ -5,6 +5,10 @@ from UserTracker.models import DailyLog, User
 
 @admin.register(DailyLog)
 class DailyLogAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "notes", "date", "slug")
+    list_display = ("id", "user", "notes", "date", "slug", "count_chars")
     list_display_links = ("user",)
     ordering = ["date", "id"]
+
+    @admin.display(description="Количество символов в заметках")
+    def count_chars(self, daily_log: DailyLog) -> str:
+        return f"{len(daily_log.notes)} символов"
